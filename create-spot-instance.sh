e800b9274b60c97cd541ff7b471363bf96e44ea3
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-16 18:30:01 +0530

    updating

--- create-spot-instance.sh
+++ create-spot-instance.sh
@@ -11,7 +11,7 @@ CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=lt-05637ecd56f2d04c6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-04f466d3e7d014d3d, Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
     UPDATE_DNS_RECORDS $1
